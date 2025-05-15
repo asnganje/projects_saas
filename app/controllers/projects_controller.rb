5c03c400f9e3986989f39a7afd3c44234362194a
asnganje <abd.nganje@gmail.com>
2025-05-15 09:52:50 +0300

    add create project action logic

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,9 +2,24 @@ class ProjectsController < ApplicationController
   layout "admin"
   before_action :authenticate_user!
   def index
-    @projects = []
-    10.times.each do |index|
-      @projects << "projects_#{index+1}"
+    @projects = Project.all
+  end
+
+  def new
+    
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if(@project.save)
+      redirect_to projects_url, notice:"Project created!"
+    else
+      redirect_to projects_url, alert:@project.errors.full_messages.join("")
     end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
