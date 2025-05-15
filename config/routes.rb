68ff5645b0208089370957d765e47977bddfafeb
asnganje <abd.nganje@gmail.com>
2025-05-15 09:52:29 +0300

    add or permit destroy into our projects route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :dashboard, only: [ :index ]
-  resources :projects, only: [:index, :create]
+  resources :projects, only: [:index, :create, :destroy]
   root "home#index"
 end
