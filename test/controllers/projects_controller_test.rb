379edabe662156dfdd4a260ad925913079f97626
asnganje <abd.nganje@gmail.com>
2025-05-15 12:31:08 +0300

    allow user in tests

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers 
+
+  setup do
+    @user = users(:one)     
+    sign_in @user           
+  end
+
   test "should get index" do
     get projects_url
     assert_response :success
   end
 end
+
