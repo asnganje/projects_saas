ca54c9663075a02594ead1a150f3000d4a250bf5
asnganje <abd.nganje@gmail.com>
2025-05-15 10:09:54 +0300

    allow all routes for projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :dashboard, only: [ :index ]
-  resources :projects, only: [:index, :create, :destroy]
+  resources :projects
   root "home#index"
 end
