122bcd86110df621e3a816948b612689bceca38b
asnganje <abd.nganje@gmail.com>
2025-05-15 09:12:40 +0300

    add create route under projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :dashboard, only: [ :index ]
-  resources :projects, only: [:index]
+  resources :projects, only: [:index, :create]
   root "home#index"
 end
