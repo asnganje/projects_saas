47f3ddda39134ae61f4f90c3713805ce1ecf0e93
asnganje <abd.nganje@gmail.com>
2025-05-15 08:50:17 +0300

    add multiple projects and modify min height setup

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,9 @@ class ProjectsController < ApplicationController
   layout "admin"
   before_action :authenticate_user!
   def index
+    @projects = []
+    10.times.each do |index|
+      @projects << "projects_#{index+1}"
+    end
   end
 end
