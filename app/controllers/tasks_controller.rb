0b93dbaef41da8693cc330a0382ffc3e3b50e7b1
asnganje <abd.nganje@gmail.com>
2025-05-16 07:29:20 +0300

    add authentication and display from dashboard

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  layout "admin"
 
   # GET /tasks or /tasks.json
   def index
