1794a6e5ae95aceaa1afa0f637e0c0a40e105299
asnganje <abd.nganje@gmail.com>
2025-05-16 18:51:01 +0300

    add stimulus javascript to update complete column db table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,6 @@ class TasksController < ApplicationController
       @task = @project.tasks.find(params[:id])
     end
     def task_params
-      params.expect(task: [ :name, :duedate, :completed_at, :priority, :project_id ])
+      params.expect(task: [ :completed, :name, :duedate, :completed_at, :priority, :project_id ])
     end
 end
