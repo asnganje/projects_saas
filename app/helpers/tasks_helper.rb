9befb165dc54b3306cffb3132c0cfa3869d47eb8
asnganje <abd.nganje@gmail.com>
2025-05-17 17:07:21 +0300

    add helper to handle styling based on task priority

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,12 @@
 module TasksHelper
+  def task_priority(priority)
+    case priority
+      when "high"
+        content_tag(:span, "High", class: "bg-green-700 rounded-full px-2 py-1")
+      when "medium"
+        content_tag(:span, "Medium", class: "bg-yellow-500 rounded-full px-2 py-1")
+      when "low"
+        content_tag(:span, "Low", class: "bg-red-500 rounded-full px-2 py-1")
+      end
+  end
 end
