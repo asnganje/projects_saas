794b253699121aa31d0c60338c91e32afac207f4
asnganje <abd.nganje@gmail.com>
2025-05-18 11:44:42 +0300

    modify routes to include notifications and read notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :notifications, only: [:index]
   get "search", to: "search#index"
+  get "read_notifications", to: "read_notifications#read_all"
   devise_for :users
   resources :dashboard, only: [ :index ]
   resources :projects do
