ddfaaf0ce1ddad5a0cc47d6f7de6cd8c751476b6
asnganje <abd.nganje@gmail.com>
2025-05-20 10:17:21 +0300

    add calendar display logic

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,5 +2,7 @@ class CalendarController < ApplicationController
   before_action :authenticate_user!
   layout "admin"
   def index
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @tasks = current_user.tasks.where(duedate: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
