ccd92e98e14db733e6270a644fe411b3fb831847
asnganje <abd.nganje@gmail.com>
2025-05-21 09:07:29 +0300

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, owned_organization_attributes: [:name, :subdomain]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name, owned_organization_attributes: [ :name, :subdomain ] ])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, owned_organization_attributes: [:name, :subdomain]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :full_name, owned_organization_attributes: [ :name, :subdomain ] ])
   end
 end
--- db/migrate/20250521051836_create_organizations.rb
+++ db/migrate/20250521051836_create_organizations.rb
@@ -3,7 +3,7 @@ class CreateOrganizations < ActiveRecord::Migration[8.0]
     create_table :organizations do |t|
       t.string :subdomain
       t.string :name
-      t.belongs_to :owner, null: false, foreign_key: {to_table: :users}
+      t.belongs_to :owner, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
