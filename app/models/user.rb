ac38c6eb3fcd43ea3b2c9ea2ed68e687835c8c54
asnganje <abd.nganje@gmail.com>
2025-05-21 09:04:22 +0300

    setup logic for adding organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
   has_many :projects
+  has_one :owned_organization, class_name: "Organization", foreign_key: :owner_id, inverse_of: :owner, dependent: :destroy
   has_many :tasks, through: :projects
   has_many :notifications, as: :recipient, dependent: :destroy, class_name: "Noticed::Notification"
+  accepts_nested_attributes_for :owned_organization, reject_if: :all_blank
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_21_051836) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_21_060317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -165,6 +165,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_21_051836) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
