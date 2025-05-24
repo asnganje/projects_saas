d16fb552b852bf0e342ab2413e917fd99cde09a0
asnganje <abd.nganje@gmail.com>
2025-05-24 10:17:24 +0300

    downcase subdomain and add link to sidebar

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,10 @@
 class Organization < ApplicationRecord
+  before_validation :downcase_subdomain
   belongs_to :owner, class_name: "User"
   has_many :users
+
+  private
+  def downcase_subdomain
+    self.subdomain = subdomain.downcase if subdomain.present?
+  end
 end
