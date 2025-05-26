e45573bf07ba3461888a920d7c1495d6c5290280
asnganje <abd.nganje@gmail.com>
2025-05-26 13:59:27 +0300

    modify project view and allow assigning project to a team

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -42,7 +42,7 @@ class ProjectsController < ApplicationController
 
   private
   def project_params
-    params.require(:project).permit(:name)
+    params.require(:project).permit(:name, :team_id)
   end
 
   def set_project
