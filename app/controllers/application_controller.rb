bd50bc9bc2f3ed906f60e27dca242bec43f7a8ec
asnganje <abd.nganje@gmail.com>
2025-05-29 23:33:33 +0300

    remove unsafe check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  skip_before_action :verify_authenticity_token
   impersonates :user
   set_current_tenant_by_subdomain(:organization, :subdomain)
   before_action :configure_permitted_parameters, if: :devise_controller?
