3f45cd71f339b6f7e00b4277d01f023ad665714a
asnganje <abd.nganje@gmail.com>
2025-06-03 06:00:53 +0300

    update csrf checker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, only: [:create]
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token, only: [:create, :login]
   impersonates :user
   set_current_tenant_by_subdomain(:organization, :subdomain)
   before_action :configure_permitted_parameters, if: :devise_controller?
