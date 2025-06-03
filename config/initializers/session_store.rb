340b308f623f4fc48e3dc67c7d8320764ea8fec6
asnganje <abd.nganje@gmail.com>
2025-06-03 05:39:10 +0300

    modify session store for production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_projects_sass_session", domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: "_projects_saas_session", domain: :all, tld_length: 1, secure: Rails.env.production?
